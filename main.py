004b87a08621755059b44d8a7fd5155e74a0770f
tomes_f <filip.tomes3@gmail.com>
2022-03-08 16:58:04 +0100

    showing all results

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-import sys
 from AxelrodPlayer import AxelrodPlayer
 
 
@@ -24,18 +23,37 @@ def always_cooperate(history):
 
 
 def tit_for_tat(history: list):
-    if len(history) == 0:
-        return 2
-    return history[-1][1]
+    return 2 if len(history) == 0 else history[-1][1]
 
 
 def random_strategy(history):
     return random.randint(1, 2)
 
 
-strategies = [always_defect, always_cooperate, tit_for_tat, random_strategy]
+def tit_for_two_tat(history):
+    if len(history) < 2:
+        return 2
+    if history[-1][1] == 1 and history[-2][1] == 1:
+        return history[-1][1]
+    return 2
+
+
+def print_result(p1: AxelrodPlayer, p2: AxelrodPlayer):
+    print(f"{player_one.strategy.__name__} vs {player_two.strategy.__name__}\n")
+    if p1.points > p2.points:
+        print(f"The winner strategy is {p1.strategy.__name__} with {p1.points} points.")
+    elif p1.points == p2.points:
+        print(f"It's draw. Both {p1.strategy.__name__} and {p2.strategy.__name__} with {p1.points} points.")
+    else:
+        print(f"The winner strategy is {p2.strategy.__name__} with {p2.points} points.\n")
+
+
+strategies = [always_defect, always_cooperate, tit_for_tat, random_strategy, tit_for_two_tat]
+stats = []
 
 for index, strategy in enumerate(strategies):
+    print("\n" + ("-" * 20))
+    print(f"Game #{index + 1}")
     for strategy_index, player_two_strategy in enumerate(strategies[index:len(strategies)]):
         moves_history = []
         player_one = AxelrodPlayer(strategy=strategy, points=0)
@@ -50,9 +68,18 @@ for index, strategy in enumerate(strategies):
             player_one.points += point_rules.get((player_one_choice, player_two_choice))
             player_two.points += point_rules.get((player_two_choice, player_one_choice))
 
-        print(f"#{strategy_index + 1} Game - {player_one.strategy} vs {player_two.strategy}")
-        print("Points (player 1): ", player_one.points)
-        print("Points (player 2): ", player_two.points)
+        stats.append(player_one)
+        stats.append(player_two)
+        print_result(player_one, player_two)
 
 
+strategy_statistics = {}
+for strategy in strategies:
+    strategy_statistics[strategy.__name__] = sum(stat.points for stat in stats if stat.strategy == strategy)
+
+print("\n\n")
+for strategy_name, strategy_points in strategy_statistics.items():
+    print(f"STRATEGY {strategy_name} earned: {strategy_points}")
+print(f"\nWinner strategy is '{max(strategy_statistics, key=strategy_statistics.get)}' "
+      f"with {max(strategy_statistics.values())} points.\n")
 
